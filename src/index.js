8fe32f75c91ab05307e49a1dd0c9069e6f8fab14
Juan Panasiti <juanpanasiti@gmail.com>
2020-12-08 00:12:38 -0300

    Instalación de bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
