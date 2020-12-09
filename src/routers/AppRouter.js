9e57fbd82bb08fe74bea1a52b2d0fd85c0fd2195
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-08 23:03:56 -0300

    Estructura de la app basica

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Switch from 'react-bootstrap/esm/Switch'
+import { Router } from 'react-router-dom'
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        
+      </div>
+      <Switch>
+
+      </Switch>
+    </Router>
+  )
+}
