1e5c14dfbf05e1eca426d80f4bc2d256e5fb0c45
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-09 00:32:45 -0300

    Agregado un segundo router

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { DcScreen } from '../components/dc/DcScreen';
+import { HeroScreen } from '../components/heroes/HeroScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { Navbar } from '../components/ui/Navbar';
+
+export const DashboardRoutes = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path='/marvel' component={MarvelScreen} />
+          <Route exact path='/dc' component={DcScreen} />
+          <Route exact path='/heroe/:heroeId' component={HeroScreen} />
+        </Switch>
+      </div>
+    </>
+  );
+};
