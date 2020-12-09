c5359b73346bee1fded44a231f89cbe8e2b51f39
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-09 00:10:16 -0300

    Configurado primer router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Switch from 'react-bootstrap/esm/Switch'
-import { Router } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { LoginScreen } from '../components/login/LoginScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { Navbar } from '../components/ui/Navbar';
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
-        
+        <Navbar />
+        <Switch>
+          <Route exact path='/login' component={LoginScreen} />
+          <Route exact path='/' component={MarvelScreen} />
+        </Switch>
       </div>
-      <Switch>
-
-      </Switch>
     </Router>
-  )
-}
+  );
+};
