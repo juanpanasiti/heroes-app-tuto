dc93cf15c91f690f6a6fc405fbaa50861672f2a6
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-10 13:33:49 -0300

    uso de replace para ir a /marvel

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+  const handleClick = () => {
+    history.replace('/marvel')
+  };
   return (
-    <div>
+    <div className='container mt-5'>
       <h1>Login Screen</h1>
+      <hr />
+
+      <button className='btn btn-outline-success' onClick={handleClick}>
+        Login
+      </button>
     </div>
   );
 };
