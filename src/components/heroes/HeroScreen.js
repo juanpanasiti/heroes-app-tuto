d4b4fc5a60ddbda4adcd7cdd83da3c80f301dab6
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-12-13 11:39:38 -0300

    Arreglada la vista del hero e implementacion de useMemo

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Button, Col, Image, Row } from 'react-bootstrap';
+import { Redirect, useParams } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
-  return (
-    <div>
-      <h1>Hero Screen</h1>
-    </div>
-  );
+export const HeroScreen = ({ history }) => {
+    const { heroeId } = useParams();
+    const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
+    // const hero = getHeroById(heroeId);
+    const { superhero, publisher, alter_ego, first_appearance, characters } = hero;
+
+    if (!hero) {
+        return <Redirect to='/' />;
+    }
+
+    const handleReturn = () => {
+        if (history.length > 2) {
+            history.goBack();
+        } else {
+            if (publisher === 'Marvel Comics') {
+                history.replace('/marvel');
+            } else {
+                history.replace('/dc');
+            }
+        }
+    };
+    return (
+        <>
+            <Row className='mt-5'>
+                <Col md={4}>
+                    <Image src={`../assets/heroes/${heroeId}.jpg`} alt={superhero} className='img-thumbnail' />
+                </Col>
+                <Col md={8}>
+                    <h3> {superhero} </h3>
+                    <ul className='list-group list-group-flush'>
+                        <li className='list-group-item'>
+                            <b> Alter ego: </b>
+                            {alter_ego}
+                        </li>
+                        <li className='list-group-item'>
+                            <b> Publisher: </b>
+                            {publisher}
+                        </li>
+                        <li className='list-group-item'>
+                            <b> First Appearance: </b>
+                            {first_appearance}
+                        </li>
+                    </ul>
+                    <h5>Characters</h5>
+                    <p>{characters}</p>
+
+                    <Button variant='outline-warning' onClick={handleReturn}>
+                        Volver
+                    </Button>
+                </Col>
+            </Row>
+        </>
+    );
 };
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import { DcScreen } from '../components/dc/DcScreen';
 import { HeroScreen } from '../components/heroes/HeroScreen';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { Navbar } from '../components/ui/Navbar';
 
 export const DashboardRoutes = () => {
-  return (
-    <>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path='/marvel' component={MarvelScreen} />
-          <Route exact path='/dc' component={DcScreen} />
-          <Route exact path='/heroe/:heroeId' component={HeroScreen} />
-        </Switch>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route exact path='/marvel' component={MarvelScreen} />
+                    <Route exact path='/dc' component={DcScreen} />
+                    <Route exact path='/hero/:heroeId' component={HeroScreen} />
+
+                    <Redirect to='/marvel' />
+                </Switch>
+            </div>
+        </>
+    );
 };
